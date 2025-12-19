496314050a1b013f29e48de0419f19cbb87f5843
govindti <108tiwari.g@gmail.com>
2025-12-19 22:19:53 +0530

    registeration done

--- src/auth/dto/registerUser.dto.ts
+++ src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 export class RegisterDto {
+    @IsString()
+    @IsNotEmpty()
     fname: string;
+
+    @IsString()
+    @IsNotEmpty()
     lname: string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
-    password: string
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
 }
